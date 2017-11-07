d41f22feb4cbc47e803d62a0605379a72c567b77
Brandon Klotz <brandonklotz@icloud.com>
2017-11-07 00:05:13 -0500

    fixed layout

--- src/GraphBar.js
+++ src/GraphBar.js
@@ -7,7 +7,7 @@ class GraphBar extends Component {
       return (
         <div className="GraphWrapper">
           <div className="GraphBarContainer">
-            <div className="GraphBar" style={{height:this.props.height*10+"px"}}><p>{this.props.height}</p></div>
+            <div className="GraphBar" style={{height:this.props.height*20+"px"}}><p>{this.props.height}</p></div>
             <div><h2>{this.props.label}</h2></div>
           </div>
         </div>
